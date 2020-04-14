0179a3e4843bea0e18b869190bbd131615c4bc9b
avrch <61111977+avrch@users.noreply.github.com>
2020-04-14 10:25:18 +0200

    retweets

--- ourfirstRfile.R
+++ ourfirstRfile.R
@@ -3,7 +3,7 @@
 
 #using the twitteR package ####
 library(twitteR)
-
+ 
 
 #From the twitter app (Keys and tokens)
 api_key <- "NZdDgX5gBv83yRILFlJiq5J1R" # API key 
@@ -16,6 +16,7 @@ setup_twitter_oauth(api_key, api_secret, token, token_secret) # setup for access
 
 tweets <- searchTwitter('#covid19uk', n=100000, lang = "en") # the function searchTwitter search for tweets based on the specified parameters
 #tweets1 = searchTwitter('#covid19uk', since = '2020-03-01', geocode = '57.1512475,-7.3292194', n=100000, lang="en") #https://rpubs.com/sumitkumar-00/twitter_sentiment_analysis, I haver not tried it, I altered the geo code to uk
+tweets <- strip_retweets(tweets)
 tweets.df <-twListToDF(tweets) # creates a data frame with one row per tweet
 
 tweetDF <- as.data.frame(tweets.df)
