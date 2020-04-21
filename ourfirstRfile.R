4fc719573844bca69a7c1f8ba98f4ec0a857158d
terezash <tereza.shanelova@gmail.com>
2020-04-21 14:16:30 +0200

    Update ourfirstRfile.R

--- ourfirstRfile.R
+++ ourfirstRfile.R
@@ -173,4 +173,94 @@ comparison.cloud(tdm1, random.order=FALSE,
                  title.size=1, max.words=250, scale=c(2.5, 0.4),rot.per=0.4)
 
 
+
+
+
+
+
+
+#adding columns with sentiment per each word!
+
+# Load dplyr and tidytext
+library(dplyr)
+library(tidytext)
+library(syuzhet)
+library(textdata)
+install.packages("textdata")
+# Choose the bing lexicon
+#get_sentiments("bing")
+get_sentiments("bing")
+
+# Choose the nrc lexicon; INSTALLED UNDER CONDITION OF CITING IT!!!!
+get_sentiments("nrc") %>%
+  count(sentiment) # Count words by sentiment
+
+sandbox <- read.csv("D:/Tereza/Aarhus university/master/2. semester/Applied Data Science/project/sandbox.csv")
+
+#a. keep only some columns 
+data <- select(sandbox, created_at, text)
+data <- data %>%
+  mutate(tweet_number = row_number())
+
+#change vector types
+data <- transform(data, text = as.character(text), created_at =as.Date(created_at))
+
+str(data)
+
+
+#tide 
+tidy <- data %>%
+  # Group by the titles of the plays
+  group_by(tweet_number) %>%
+  # Transform the non-tidy text data to tidy text data
+  unnest_tokens(word, text) %>%
+  ungroup()
+
+# Pipe the tidy data frame to the next line
+tidy %>% 
+  # Use count to find out how many times each word is used
+  count(word, sort = TRUE)
+
+
+#adding column with sentiment - so it should be theoretically possible with emotion? 
+sentiment <- tidy %>%
+  # Implement sentiment analysis with the "bing" lexicon
+  inner_join(get_sentiments("bing"))
+
+
+#this code adds sentiment to column (of words, not whole tweets)
+sentiment %>%
+  # Find how many positive/negative words each tweet (so here it is tweet number) has
+  group_by(tweet_number) %>%
+  count(tweet_number,sentiment)
+View(sentiment)
+
+
+
+
+word_counts <- tidy %>%
+  # Implement sentiment analysis using the "bing" lexicon
+  inner_join(get_sentiments("bing")) %>%
+  # Count by word and sentiment
+  count(word,sentiment)
+
+top_words <- word_counts %>%
+  # Group by sentiment
+  group_by(sentiment) %>%
+  # Take the top 10 for each sentiment
+  top_n(10) %>%
+  ungroup() %>%
+  # Make word a factor in order of n
+  mutate(word = reorder(word, n))
+
+# Use aes() to put words on the x-axis and n on the y-axis
+ggplot(top_words, aes(word, n, fill = sentiment)) +
+  # Make a bar chart with geom_col()
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free") +  
+  coord_flip()
+
+
+
+
 #next step try to determine emotions from ngrams!
